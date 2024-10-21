1e1ddf17cfb2740945f7db6e6e22ef2458de4db8
TATOAO <liangwentaoss@126.com>
2024-10-21 11:49:36 +0800

    python re yaml fastapi

--- 软件开发/Fastapi/jwt_expire.py
+++ 软件开发/Fastapi/jwt_expire.py
@@ -1,33 +1,36 @@
+
+# jwt_expire.py
 from datetime import datetime, timedelta, timezone
 import time
 import jwt
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 1
-SECRET_KEY = "ceff06d79480f619d6eb03ca56de53be7444c189da9462c712aae356c98d8590"
+SECRET_KEY = "xxxxx"
 ALGORITHM = "HS256"
 
 
 data: dict ={"sub": 'alan'}
 to_encode = data.copy()
 expire = datetime.now(timezone.utc) + timedelta(seconds=5)
-to_encode.update({"sss": expire})
+
+# 这是一个固定的入产 exp
+to_encode.update({"exp": expire})
+# 如果没有会立马报错
+# to_encode.update({"ss": expire})
 
 encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 for i in range(20):
+
     payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=[ALGORITHM])
+    # 超过expire 时间，jwt.decode 会立马报错
     print(encoded_jwt)
     print(payload)
     time.sleep(0.5)
 
+    # raise ExpiredSignatureError("Signature has expired")
+    # jwt.exceptions.ExpiredSignatureError: Signature has expired
 
 
-
-
-# 软件开发/Fastapi/hello_world.py
-# python hello_world.py
-# if __name__ == "__main__":
-#     main()
-
-
+# cd 软件开发/Fastapi && python jwt_expire.md && cd - 
---
+++ 软件开发/Fastapi/security_my_4.py
@@ -0,0 +1,56 @@
+
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+# to get a string like this run:
+# openssl rand -hex 32
+# SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = "ceff06d79480f619d6eb03ca56de53be7444c189da9462c712aae356c98d8590"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
