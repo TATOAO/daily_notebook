22dc2969ec1cf352c806f783421cc6b9b2c4aa3c
TATOAO <liangwentaoss@126.com>
2024-12-02 16:49:32 +0800

    binery tree

--- 算法/算法题/树/二叉树.py
+++ 算法/算法题/树/二叉树.py
@@ -13,36 +13,59 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def __repr__(self):
+        return str(self.val)
+
+    def __str__(self):
+        return str(self.val)
+
+class RecordesTreeNode(TreeNode):
+    def __init__(self, val=0, left=None, right=None):
+        super().__init__(val=0, left=None, right=None)
+        self.left_num = 0
+        self.right_num = 0
+        
 class CBTInserter:
 
-    class CBT:
-        def __init__(self, val=0, left=None, right=None):
-            self.val = val
-            self.left = left
-            self.right = right
-            self.left_nodes
-
-
-
-
-
     def __init__(self, root: TreeNode):
-        self.root = root
-        pass
-
+        self.root = RecordesTreeNode(val = root.val)
 
     def insert(self, v: int) -> int:
+        temp_node: RecordesTreeNode | None = self.root
+
+        new_node = RecordesTreeNode(val=v)
+        
+        while temp_node:
+            if v < temp_node.val:
+                temp_node.left_num += 1
+                if temp_node.left == None:
+                    temp_node.left = new_node
+
+                temp_node = temp_node.left
+
+            else:
+                temp_node.right_num += 1
+                if temp_node.right == None:
+                    temp_node.right = new_node
+
+                temp_node = temp_node.right
+            
         return 0
 
-
     def get_root(self) -> TreeNode:
-        return TreeNode()
+        return self.root
+
+    
 
 
 
 # Your CBTInserter object will be instantiated and called as such:
-# obj = CBTInserter(root)
-# param_1 = obj.insert(v)
-# param_2 = obj.get_root()
+root = TreeNode(0)
+obj = CBTInserter(root)
+param_1 = obj.insert(3)
+param_2 = obj.get_root()
+import ipdb;ipdb.set_trace()
+print('xxx')
+
 
 
